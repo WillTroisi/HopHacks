ac24ba5593a937c1b0b0582b42a61d02c8f3395e
fcostell0 <123022605+fcostell0@users.noreply.github.com>
2024-09-14 17:02:16 -0400

    Finished visualization script

---
+++ visualization/plotScript.R
@@ -0,0 +1,44 @@
+library(bdgramR)
+library(ggplot2)
+library(dplyr)
+
+df <- read.csv('setVis.csv')
+
+outputFilename <- colnames(df)[1]
+colnames(df) <- c("muscleGroup", "sets")
+df <- df[-1,]
+
+df <- df %>%
+  mutate(muscleGroup = case_when(
+    muscleGroup == "neck" ~ "Neck",
+    muscleGroup == "forearms" ~ "Forearm",
+    muscleGroup == "upper back" ~ "Upper Back",
+    muscleGroup == "lower back" ~ "Lower_Back",
+    muscleGroup == "glutes" ~ "Gluteus",
+    muscleGroup == "hamstrings" ~ "Hamstrings",
+    muscleGroup == "quads" ~ "Quadriceps",
+    muscleGroup == "calves" ~ "Calves",
+    muscleGroup == "pecs" ~ "Pectoral",
+    muscleGroup == "abs" ~ "Abdominals",
+    muscleGroup == "shoulders" ~ "Arm",
+    TRUE ~ muscleGroup
+  ))
+
+dat <- bdgramr(data, "original_male")
+
+dat <- dat %>%
+  left_join(df, by = c("Group" = "muscleGroup"))
+
+
+
+dat$sets <- as.numeric(dat$sets)
+
+
+plot <- ggplot(dat, aes(x,y, group = Id)) + 
+  geom_bdgramr(color = "black", aes(fill = sets)) +
+  scale_fill_gradient(low = 'blue', high = 'red')
+
+png(outputFilename)
+print(plot)
+dev.off()
+
