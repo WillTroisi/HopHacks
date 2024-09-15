43a8204ec7d9077efb789e65132238e259c360c1
Manuel Mateo <yekayama@gmail.com>
2024-09-15 07:23:22 -0400

    remove key
    remove secret key (LOL)

--- backend/houndfit/houndfit/settings.py
+++ backend/houndfit/houndfit/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%o@c+y#7tklpytpvhv#is7g8wl4()-e8h8*hdt#@@&z26(@%v-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
